522e3628923384e72a532a6ab7e49bde7f496b4f
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2024-01-02 18:43:39 -0500

    feat: parallel readers; noel -> get_lengths

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! A small library for computing the number of base pairs in a gene
 //!
 //! to include noel as a library and use it within your project follow these steps:
-//! 1. include `noel = 0.1.0` or `noel = "*"` under `[dependencies]` in your `Cargo.toml` file or just run `cargo add noel` from the command line
+//! 1. include `noel = 0.2.1` or `noel = "*"` under `[dependencies]` in your `Cargo.toml` file or just run `cargo add noel` from the command line
 //! 2. the library name is `noel`, to use it just write:
 //! ``` rust
 //! use noel::{noel, noel_reader};
@@ -15,72 +15,90 @@
 //! let exons = noel_reader(input: &PathBuf)
 //! let lengths: HashMap<String, u32> = noel(exons: HashMap<String, Vec<(u32, u32)>>)
 //! ```
-//!
 
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::path::{Path, PathBuf};
+use std::io::{self, Read};
+use std::path::PathBuf;
+
+use rayon::prelude::*;
 
 pub mod record;
 pub use record::*;
 
-pub fn noel(exons: HashMap<String, Vec<(u32, u32)>>) -> Option<HashMap<String, u32>> {
-    let mut genes = HashMap::new();
-    for (gene, exons) in exons.iter() {
-        let (min_start, max_end) = exons.iter().fold((u32::MAX, 0), |acc, &(start, end)| {
-            (acc.0.min(start), acc.1.max(end))
-        });
+pub fn get_lengths(exons: HashMap<String, Vec<(u32, u32)>>) -> Option<Vec<(String, u32)>> {
+    let lengths = exons
+        .into_par_iter()
+        .map(|(gene, exons)| {
+            let (min_start, max_end) = exons.iter().fold((u32::MAX, 0), |acc, &(start, end)| {
+                (acc.0.min(start), acc.1.max(end))
+            });
 
-        let mut bp = vec![0; (max_end - min_start + 1) as usize];
+            let mut bp = vec![0; (max_end - min_start + 1) as usize];
 
-        for &(start, end) in exons.iter() {
-            for i in (start - min_start)..(end - min_start + 1) {
-                bp[i as usize] = 1;
+            for &(start, end) in exons.iter() {
+                for i in (start - min_start)..(end - min_start + 1) {
+                    bp[i as usize] = 1;
+                }
             }
-        }
 
-        let total_bp: u32 = bp.iter().sum();
-        genes.insert(gene.clone(), total_bp);
-    }
-    Some(genes)
+            let total_bp: u32 = bp.iter().sum();
+            (gene, total_bp)
+        })
+        .collect::<Vec<(String, u32)>>();
+
+    Some(lengths)
 }
 
 pub fn noel_reader(input: &PathBuf) -> Option<HashMap<String, Vec<(u32, u32)>>> {
-    let rdr = read_gtf(input)?;
-    let mut exons: HashMap<String, Vec<(u32, u32)>> = HashMap::new();
+    let contents = reader(input).unwrap();
+    let records = parallel_parse(&contents).unwrap();
 
-    for line in rdr.lines() {
-        let line = line.unwrap();
-
-        if line.starts_with("#") {
-            continue;
-        }
-
-        match Record::new(line) {
-            Ok(record) => {
-                let (start, end, gene_id) = record.info();
+    let exons: HashMap<String, Vec<(u32, u32)>> = records
+        .into_par_iter()
+        .filter(|line| line.feature() == "exon") // Filter out non-exons
+        .fold(
+            || HashMap::new(),
+            |mut map, line| {
+                map.entry(line.gene_id)
+                    .or_insert_with(Vec::new)
+                    .push((line.start, line.end));
+                map
+            },
+        )
+        .reduce(
+            || HashMap::new(),
+            |mut exons, thread_map| {
+                for (key, value) in thread_map {
+                    exons.entry(key).or_insert_with(Vec::new).extend(value);
+                }
                 exons
-                    .entry(gene_id)
-                    .or_insert(Vec::new())
-                    .push((start, end));
-            }
-            Err(_) => continue,
-        }
-    }
+            },
+        );
+
     Some(exons)
 }
 
-pub fn read_gtf(input: &Path) -> Option<BufReader<File>> {
-    match input.extension() {
-        Some(ext) if ext == "gtf" || ext == "gff" || ext == "gff3" => {
-            let records = BufReader::new(File::open(input).unwrap());
-            Some(records)
-        }
-        _ => {
-            let err = "No gtf/gff file provided. Check the extension of your file.";
-            eprintln!("{} {}", "Error:", err);
-            None
-        }
-    }
+fn reader(file: &PathBuf) -> io::Result<String> {
+    let mut file = File::open(file)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn parallel_parse<'a>(s: &'a str) -> Result<Vec<Record>, NoelError> {
+    let records: Result<Vec<Record>, NoelError> = s
+        .par_lines()
+        .map(|line| match Record::new(line) {
+            Ok(record) => Ok(record),
+            Err(_) => Ok(Record {
+                feat: "".to_string(),
+                start: 0,
+                end: 0,
+                gene_id: "".to_string(),
+            }),
+        })
+        .collect();
+
+    return records;
 }
